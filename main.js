2ce68a3bab48784816478031c6639e9b436c1f8c
Samuel Styles <samuelks82406@gmail.com>
2024-05-21 16:21:48 -0400

    afsdfdsdsd

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Optional: Use soft shadows
 document.body.appendChild(renderer.domElement);
+
+
 
 // Adding OrbitControls
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = .5;
 controls.screenSpacePanning = false;
-controls.maxPolarAngle = Math.PI / 2.5;
+// controls.maxPolarAngle = Math.PI / 2.5;
 controls.maxDistance = 20;
 controls.minDistance = 2.5;
 controls.enablePan = true;
@@ -24,21 +28,64 @@ controls.touches = {
 	TWO: THREE.TOUCH.DOLLY
 }
 
-// Adding lights
-const ambientLight = new THREE.AmbientLight(0x404040, 1); // Soft white light
-scene.add(ambientLight);
+// // Adding lights
+// const ambientLight = new THREE.AmbientLight(0x404040, 1); // Soft white light
+// scene.add(ambientLight);
 
-const hemiLight = new THREE.HemisphereLight( 0xB1D9D9, 0xBF9000, 1 ); 
-hemiLight.position.set(20, 100, 20);
-scene.add(hemiLight);
+// const hemiLight = new THREE.HemisphereLight( 0xB1D9D9, 0xf2d57c, 1 ); 
+// hemiLight.position.set(20, 100, 20);
+// scene.add(hemiLight);
 
-const directLight = new THREE.DirectionalLight(0xffffff, 2.5);
-directLight.position.set(40, 100, 20);
-directLight.castShadow = true;
-scene.add(directLight);
+// const directLight = new THREE.DirectionalLight(0xffffff, 2.5);
+// directLight.position.set(40, 100, 20);
+// directLight.castShadow = true;
+// scene.add(directLight);
 
 //Fog
-scene.fog = new THREE.Fog( 0xc7f7f7, 10, 100 );
+// scene.fog = new THREE.Fog( 0xc7f7f7, 10, 100 );
+
+// Adding lights
+const ambientLight = new THREE.AmbientLight(0xffffff, .7); // Soft white light
+scene.add(ambientLight);
+
+const light = new THREE.DirectionalLight( 0xffffff, 2 );
+light.position.set( 1, 5, 2 ); //default; light shining from top
+light.castShadow = true; // default false
+
+light.shadow.camera.left = -10;
+light.shadow.camera.right = 10;
+light.shadow.camera.top = 10;
+light.shadow.camera.bottom = -10;
+light.shadow.mapSize.width = 1000; // default
+light.shadow.mapSize.height = 1000; // default
+light.shadow.camera.near = 0.1; // default
+light.shadow.camera.far = 500; // default
+// light.radius= 4;
+
+scene.add( light );
+
+const sphereGeometry = new THREE.SphereGeometry( 1, 5, 5 );
+const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+sphere.castShadow = true; //default is false
+sphere.receiveShadow = false; //default
+scene.add( sphere );
+
+
+// const groundGeometry = new THREE.PlaneGeometry(200, 200, 1, 1);
+// const groundMaterial = new THREE.MeshStandardMaterial({ color: 0xd2b48c });
+// const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+// ground.rotation.x = -Math.PI / 2;
+// ground.receiveShadow = true; // Enable receiving shadows
+// scene.add(ground);
+
+
+
+// // Adding sky (optional)
+// const skyGeometry = new THREE.SphereGeometry(1000, 32, 32);
+// const skyMaterial = new THREE.MeshBasicMaterial({ color: 0xfffafa, side: THREE.BackSide });
+// const sky = new THREE.Mesh(skyGeometry, skyMaterial);
+// scene.add(sky);
 
 // Function to load GLTF Models
 const loader = new THREE.GLTFLoader();
@@ -48,13 +95,23 @@ const models = [];
 loader.load('assets/ground.glb', function (gltf) {
     const ground = gltf.scene;
     ground.position.set(0, 0, 0); // Position the ground slightly below the models
+    ground.receiveShadow = true; // Enable receiving shadows
+    ground.traverse( child =>
+        {
+            if( child.isMesh )
+            {
+                child.receiveShadow = true;
+            }
+        } );
     scene.add(ground);
 });
-loader.load('assets/sky.glb', function (gltf) {
-    const sky = gltf.scene;
-    sky.position.set(0, 0, 0); // Position the ground slightly below the models
-    scene.add(sky);
-});
+
+
+// loader.load('assets/sky.glb', function (gltf) {
+//     const sky = gltf.scene;
+//     sky.position.set(0, 0, 0); // Position the ground slightly below the models
+//     scene.add(sky);
+// });
 loader.load('assets/lake.glb', function (gltf) {
     const lake = gltf.scene;
     lake.position.set(0, 0, 0); // Position the ground slightly below the models
@@ -67,6 +124,14 @@ for (let i = 1; i <= 10; i++) {
         const model = gltf.scene;
         model.userData = { id: i };  // Store an ID in the model's userData
         // model.position.set((i - 5) * 3, 0, 0);  // Position the models
+          model.traverse( child =>
+            {
+                if( child.isMesh )
+                {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
+            } );
         models.push(model);
         scene.add(model);
     });
@@ -76,6 +141,13 @@ for (let i = 1; i <= 10; i++) {
         const struct = gltf.scene;
         struct.userData = { id: i };  // Store an ID in the model's userData
         // model.position.set((i - 5) * 3, 0, 0);  // Position the models
+        struct.traverse( child =>
+            {
+                if( child.isMesh )
+                {
+                    child.castShadow = true;
+                }
+            } );
         models.push(struct);
         scene.add(struct);
     });
